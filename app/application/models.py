bd3e21d55349535f1725e6ab58d3bb1059a93530
Joshua Cordeiro-Zebkowitz <zebkowitz@gmail.com>
2022-03-24 22:58:34 -0600

    Update db model

--- app/application/models.py
+++ app/application/models.py
@@ -2,15 +2,17 @@ from . import db
 from datetime import datetime
 
 
-class Todo(db.Model):
+class Food_Item(db.Model):
 
     __tablename__ = 'Food Items'
 
     id = db.Column(db.Integer, primary_key= True)
 
+    name = db.Column(db.String(50), nullable = False)
+
     content = db.Column(db.String(200), nullable = False)
-    
+
     date_created = db.Column(db.DateTime, default = datetime.utcnow)
 
     def __repr__(self):
-        return '<Task %r>' % self.id
+        return '<Food Item %r>' % self.id
