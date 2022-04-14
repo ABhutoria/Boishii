4545a72cf5bbbcaec2adaf0a9d79348924d9b322
Anay <68079961+ABhutoria@users.noreply.github.com>
2022-04-13 20:58:37 -0600

    janky js

--- app/application/static/js/getItem.js
+++ app/application/static/js/getItem.js
@@ -17,3 +17,22 @@
 })
 .then(resp => resp.json())
 .then(data => document.body.innerHTML = "<h1>" + data.type + "</h1>") */
+
+
+function hey(){
+
+    fetch('http://localhost:5000/getItem',{
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+            "Name": "Nachos"
+        })
+    }).then(response => 
+        response.json().then(data => ({
+            data: data,
+        status: response.status
+    })
+    ).then(res => {
+        document.getElementById("Nachos").innerHTML = res.data.Description;
+    }));
+}
